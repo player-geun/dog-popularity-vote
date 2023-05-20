a342bc33481cc7c84d4e46224bf8794e82a02135
player-geun <player_geun@naver.com>
2023-05-20 10:23:06 +0900

    feat : 프로젝트 초기화

---
+++ src/main/java/com/dogpopularityvote/DogPopularityVoteApplication.java
@@ -0,0 +1,13 @@
+package com.dogpopularityvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogPopularityVoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogPopularityVoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dogpopularityvote/DogPopularityVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogpopularityvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogPopularityVoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
