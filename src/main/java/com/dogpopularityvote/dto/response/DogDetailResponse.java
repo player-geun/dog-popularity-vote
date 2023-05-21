c2de6c022e4d731a11332e402be354e1fcf728f8
player-geun <player_geun@naver.com>
2023-05-21 19:06:53 +0900

    feat : Dog의 상세정보를 매핑하는 DTO 구현

---
+++ src/main/java/com/dogpopularityvote/dto/response/DogDetailResponse.java
@@ -0,0 +1,28 @@
+package com.dogpopularityvote.dto.response;
+
+import com.dogpopularityvote.domain.Dog;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class DogDetailResponse implements Serializable {
+
+    private Long id;
+    private String name;
+    private String photoName;
+    private String photoPath;
+    private int voteCount;
+
+    public DogDetailResponse(Dog dog) {
+        this.id = dog.getId();
+        this.name = dog.getName();
+        this.photoName = dog.getPhotoName();
+        this.photoPath = dog.getPhotoPath();
+        this.voteCount = dog.getVoteCount();
+    }
+}
