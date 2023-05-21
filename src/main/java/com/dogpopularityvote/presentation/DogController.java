53cae40d1ab4beb005690b2178fb3d4d1770ba83
player-geun <player_geun@naver.com>
2023-05-22 08:16:45 +0900

    feat : 카프카 요청 메세지를 생성하는 컨트롤러 기능 구현

--- src/main/java/com/dogpopularityvote/presentation/DogController.java
+++ src/main/java/com/dogpopularityvote/presentation/DogController.java
@@ -1,6 +1,8 @@
 package com.dogpopularityvote.presentation;
 
+import com.dogpopularityvote.application.DogProducerService;
 import com.dogpopularityvote.application.DogService;
+import com.dogpopularityvote.dto.request.DogVoteRequest;
 import com.dogpopularityvote.dto.response.DogDetailResponse;
 import com.dogpopularityvote.dto.response.DogInfiniteScrollResponse;
 import lombok.RequiredArgsConstructor;
@@ -8,10 +10,8 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/dogs")
 @RequiredArgsConstructor
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DogController {
 
     private final DogService dogService;
+    private final DogProducerService dogProducerService;
 
     @GetMapping
     @Cacheable(cacheNames = "dogs")
@@ -27,9 +28,15 @@ public class DogController {
         return dogService.findAllWithPage(pageable);
     }
 
-    @GetMapping("{id}/details")
+    @GetMapping("/{id}/details")
     @Cacheable(cacheNames = "dog", key = "#id")
     public DogDetailResponse findDetailById(@PathVariable Long id) {
         return dogService.findDetailById(id);
+    }
+
+    @PostMapping("/votes")
+    public ResponseEntity<Void> sendVote(@RequestBody DogVoteRequest request) {
+        dogProducerService.sendVote(request);
+        return ResponseEntity.noContent().build();
     }
 }
