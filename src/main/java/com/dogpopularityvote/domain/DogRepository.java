4b2b8f56a91511c973e9d63a285a4cb0a23f433d
player-geun <player_geun@naver.com>
2023-05-21 19:06:23 +0900

    feat : Dog의 Id를 통해 Dog를 조회하는 기능 구현

--- src/main/java/com/dogpopularityvote/domain/DogRepository.java
+++ src/main/java/com/dogpopularityvote/domain/DogRepository.java
@@ -1,5 +1,6 @@
 package com.dogpopularityvote.domain;
 
+import com.dogpopularityvote.exception.NoSuchDogException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,9 @@ import org.springframework.stereotype.Repository;
 public interface DogRepository extends JpaRepository<Dog, Long> {
 
     Slice<Dog> findAllBy(Pageable pageable);
+
+    default Dog getById(Long id) {
+        return findById(id)
+                .orElseThrow(NoSuchDogException::new);
+    }
 }
