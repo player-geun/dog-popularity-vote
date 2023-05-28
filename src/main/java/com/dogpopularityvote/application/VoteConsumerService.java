40a7e66f4eaff1445486ac96b66ea0587c6ef2c8
player-geun <player_geun@naver.com>
2023-05-28 19:19:04 +0900

    refactor : 투표값이 업데이트 되면 캐시도 업데이트 되도록 변경

--- src/main/java/com/dogpopularityvote/application/VoteConsumerService.java
+++ src/main/java/com/dogpopularityvote/application/VoteConsumerService.java
@@ -5,8 +5,10 @@ import com.dogpopularityvote.domain.DogRepository;
 import com.dogpopularityvote.domain.Vote;
 import com.dogpopularityvote.domain.VoteRepository;
 import com.dogpopularityvote.dto.request.DogVoteRequest;
+import com.dogpopularityvote.dto.response.DogDetailResponse;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +22,7 @@ public class VoteConsumerService {
 
     private final VoteRepository voteRepository;
     private final DogRepository dogRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @Transactional
     @KafkaListener(topics = "vote", groupId = "manager")
@@ -30,9 +33,11 @@ public class VoteConsumerService {
         if (vote.isEmpty()) {
             Vote savedVote = voteRepository.save(new Vote(dog, false, request.getClientIp()));
             dog.vote(savedVote, request.isVoted());
+            redisTemplate.opsForValue().set("dog::" + dog.getId(), new DogDetailResponse(dog));
             return;
         }
 
         dog.vote(vote.get(), request.isVoted());
+        redisTemplate.opsForValue().set("dog::" + dog.getId(), new DogDetailResponse(dog));
     }
 }
